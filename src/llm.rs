e94a6328d49ff9325a41ca3268bcc6e498decc7f
KP <kp-automata@protonmail.com>
2025-02-02 00:09:14 -0500

    llama, tweaked prompt

--- src/llm.rs
+++ src/llm.rs
@@ -3,7 +3,7 @@ use serde_json::json;
 use std::env;
 use dotenv::dotenv;
 
-pub fn get_inference(input: &str) -> Result<String, reqwest::Error> {
+pub fn get_inference(input: &String) -> Result<String, reqwest::Error> {
     dotenv().ok();
     let api_key = env::var("TOGETHER_API_KEY").expect("TOGETHER_API_KEY must be set");
 
@@ -11,11 +11,11 @@ pub fn get_inference(input: &str) -> Result<String, reqwest::Error> {
         .post("https://api.together.xyz/v1/chat/completions")
         .header("Authorization", format!("Bearer {}", api_key))
         .json(&json!({
-            "model": "mistralai/Mixtral-8x7B-Instruct-v0.1",
-
-            "messages": [{"role": "user", "content": input}, {"role": "system", "content": "You are a threat AI detection model. You are being given suspicious packet data. Give the user context briefly"},],
+            // "model": "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            "model": "meta-llama/Llama-3-8b-chat-hf",
+            "messages": [{"role": "user", "content": input}, {"role": "system", "content": "You are a threat AI detection model. You are being given potentially suspicious packet data. Analyze the incoming address with external internet sources"},],
             "temperature": 0.7,
-            "max_tokens": 1024
+            "max_tokens": 128
         }))
         .send()?
         .json::<serde_json::Value>()?;
@@ -30,8 +30,8 @@ pub fn get_inference(input: &str) -> Result<String, reqwest::Error> {
         .to_string())
 }
 
-pub fn run(input: &str) {
-    match get_inference(input) {
+pub fn run(input: String) {
+    match get_inference(&input) {
         Ok(response) => println!("{}", response),
         Err(err) => eprintln!("Error: {}", err),
     }
